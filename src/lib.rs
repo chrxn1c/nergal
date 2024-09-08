4dc320afaebb8cf61d17972f932dabb3e26678b5
chrxn1c <rivalofficialbusiness@outlook.com>
2024-09-09 00:23:59 +0300

    search implemented

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use std::error::Error;
+
 use std::fs;
 
 pub struct Config {
@@ -9,7 +10,7 @@ pub struct Config {
 impl Config {
     pub fn build(args: &[String]) -> Result<Config, &'static str> {
         if args.len() != 3 {
-            return Err("needed 2 command-line arguments")
+            return Err("needed 2 command-line arguments");
         }
 
         Ok(Config {
@@ -21,6 +22,37 @@ impl Config {
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
+
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
     Ok(())
 }
 
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut found_lines = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            found_lines.push(line);
+        }
+    }
+
+    found_lines
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "Charon";
+        let contents = "\
+            No coins for Charon, unable to cross over worlds
+            Dumped out in the river, found unworthy to pass now";
+        assert_eq!(
+            vec!["No coins for Charon, unable to cross over worlds"],
+            search(query, contents)
+        );
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,23 @@
+use nergal::run;
+use nergal::Config;
+
 use std::env;
 use std::process;
-use nergal::Config;
-use nergal::run;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-       println!("Incorrect input: {err}\n\
-       Terminating...");
+        println!(
+            "Incorrect input: {err}\n\
+       Terminating..."
+        );
         process::exit(1);
     });
-    println!("Searching for query {} in file {}...", config.query, config.file_path);
+    println!(
+        "Searching for query {} in file {}...",
+        config.query, config.file_path
+    );
 
     if let Err(e) = run(config) {
         println!("Application error: {e}");
