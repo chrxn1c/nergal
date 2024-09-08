881c0db04b6ac05b8edc6241d257805822d23576
chrxn1c <rivalofficialbusiness@outlook.com>
2024-09-08 21:55:54 +0300

    before refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() != 3 {
@@ -8,5 +9,8 @@ fn main() {
 
     let (query, file_path) = (&args[1], &args[2]);
 
-    println!("Searching for query {query} in file {file_path}...")
+    println!("Searching for query {query} in file {file_path}...");
+
+    let contents = fs::read_to_string(file_path).expect("File path is not located.");
+    println!("contents:\n\n {}", contents);
 }
