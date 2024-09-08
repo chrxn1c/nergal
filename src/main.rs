2305fdaa88c597c200efead87dcd4219d287cec6
chrxn1c <rivalofficialbusiness@outlook.com>
2024-09-08 22:50:12 +0300

    moved the stuff into lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::env;
-use std::fs;
 use std::process;
+use nergal::Config;
+use nergal::run;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -11,24 +13,8 @@ fn main() {
     });
     println!("Searching for query {} in file {}...", config.query, config.file_path);
 
-    let contents = fs::read_to_string(config.file_path).expect("File path is not located.");
-    println!("contents:\n\n {}", contents);
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() != 3 {
-            return Err("needed 2 command-line arguments")
-        }
-
-        Ok(Config {
-            query: args[1].clone(),
-            file_path: args[2].clone(),
-        })
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(2);
     }
 }
