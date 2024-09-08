4b77bf753ec5bf1b145abf6329ce321e6468755a
chrxn1c <rivalofficialbusiness@outlook.com>
2024-09-08 21:35:13 +0300

    feat: args with error-handling added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 3 {
+        panic!("Incorrect input: You were supposed to type in 2 command-like arguments: query and file path.\n\
+        Terminating...");
+    }
+
+    let (query, file_path) = (&args[1], &args[2]);
+
+    println!("Searching for query {query} in file {file_path}...")
 }
